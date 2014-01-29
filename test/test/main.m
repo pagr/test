84e6fcfdf470c705538c1adb87e956b1d1b3e428
Paul <paul@Pauls-Mac.local>
2014-01-29 11:53:03 +0100

    First commit

---
+++ test/test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Paul on 29/01/14.
+//  Copyright (c) 2014 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "xyzAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([xyzAppDelegate class]));
+    }
+}
---
+++ test/test/xyzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xyzAppDelegate.h
+//  test
+//
+//  Created by Paul on 29/01/14.
+//  Copyright (c) 2014 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xyzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
